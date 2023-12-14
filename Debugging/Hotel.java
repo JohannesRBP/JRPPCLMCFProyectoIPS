55c36c154401fb536177bdd124d010df756531e9
Miguelkoro <miguelkoro@gmail.com>
2023-12-14 22:57:28 +0100

    anadido generador java

---
+++ Debugging/Hotel.java
@@ -0,0 +1,18 @@
+public class Hotel{
+	private String nombre;
+	private String direccion;
+	public Hotel(){
+	}
+	public String nombre(){
+		return this.nombre
+	}
+	public void nombre(String nombre){
+		this.nombre = nombre
+	}
+	public String direccion(){
+		return this.direccion
+	}
+	public void direccion(String direccion){
+		this.direccion = direccion
+	}
+}
