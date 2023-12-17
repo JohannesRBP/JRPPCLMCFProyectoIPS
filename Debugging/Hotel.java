6fc6f42e84b2b0ab41bb7b805aa199a0f69eb3ec
Miguelkoro <miguelkoro@gmail.com>
2023-12-17 14:15:53 +0100

    Añadidos atributos clave a java y altaClase

--- Debugging/Hotel.java
+++ Debugging/Hotel.java
@@ -1,7 +1,14 @@
 public class Hotel{
+	private Int idHotel;
 	private String nombre;
 	private String direccion;
 	public Hotel(){
+	}
+	public Int idHotel(){
+		return this.idHotel
+	}
+	public void idHotel(Int idHotel){
+		this.idHotel = idHotel
 	}
 	public String nombre(){
 		return this.nombre
